edbb6a73562c1519799a8b1d578f5efd5c30928d
Leikanger <leikange@gmail.com>
2016-01-27 15:06:14 +0100

    Har lagt til god Makefile med Boost.Test kjøring

--- Makefile
+++ Makefile
@@ -1,5 +1,25 @@
-test: *cpp *h
-	g++ -std=c++11 -lboost_unit_test_framework -lboost_filesystem serialInterfaceTEST.cpp serialInterface.cpp -o testAll.out && ./testAll.out
+CC=g++
+CFLAGS=-std=c++11 -Wall -lpthread
+LIBR=-lboost_system -lboost_thread
+
+all: test
+
+runTests: test
+	./testAll.out
+
+test: serialInterface.o serialInterfaceTEST.cpp
+	$(CC) $(CFLAGS) $(LIBR) -lboost_unit_test_framework serialInterfaceTEST.cpp serialInterface.o -o testAll.out
+
+serialInterface.o: serialInterface.cpp serialInterface.h
+	$(CC) $(CFLAGS) $(LIBR) serialInterface.cpp -c
+
+
+
+test_old_all_files: *cpp *h
+	$(CC) $(CFLAGS) -lboost_unit_test_framework $(LIBR) serialInterfaceTEST.cpp serialInterface.cpp -o testAll.out 
+
+clean:
+	rm *.out *.o
 
 #serial: serialInterface.cpp serialInterface.h
 #	clang++ serialInterface.cpp
--- serialInterfaceTEST.cpp
+++ serialInterfaceTEST.cpp
@@ -1,13 +1,13 @@
 #define BOOST_TEST_MODULE serial_communication
 #define BOOST_TEST_DYN_LINK
-#include <boost/test/included/unit_test.hpp>
+#include <boost/test/unit_test.hpp>
 
 #include "serialInterface.h"
 
 using std::cout;
 namespace utf = boost::unit_test;
 
-BOOST_AUTO_TEST_SUITE(serial_communication);
+BOOST_AUTO_TEST_SUITE(MOCK_serial_communication);
 BOOST_AUTO_TEST_CASE( jeje )
 {
     BOOST_CHECK_EQUAL( 9, 9 ); 
@@ -21,12 +21,24 @@ BOOST_AUTO_TEST_CASE( constructing )
 
 BOOST_AUTO_TEST_CASE( mockSendAndReceive )
 {
-    MockSerial serial;
-
-    serial.sendMessage("melding");
-    BOOST_CHECK_EQUAL("melding", serial.getMessage());
+    MockSerial serialMock;
+    serialMock.sendMessage("melding");
+    BOOST_CHECK_EQUAL("melding", serialMock.getMessage());
 }
+BOOST_AUTO_TEST_SUITE_END();
 
+
+
+
+/*
+BOOST_AUTO_TEST_SUITE( Arduino_serial_communication );
+BOOST_AUTO_TEST_CASE( send_and_receive )
+{
+    Serial realArduino("/dev/ttyASM1");
+    realArduino.sendMessage("fraSoftware");
+}
+BOOST_AUTO_TEST_SUITE_END();
+*/
 // TODO hugs å finne/respondere for exception ved feil filepath! TODO Ved
 // construction av Serial object!
 // TODO
@@ -40,4 +52,3 @@ BOOST_AUTO_TEST_CASE( Serial_send_and_receive )
     BOOST_CHECK_EQUAL("asdf", mockReadInterface.getMessage());
 }
 */
-BOOST_AUTO_TEST_SUITE_END();
